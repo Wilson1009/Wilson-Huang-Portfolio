8d3aaaf3455566db371617d155cbc95aa478d8ec
Wilson1009 <wwss1009@gmail.com>
2026-01-10 23:57:55 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Wilson-Huang-Portfolio/', 
+  base: '/wilson-huang-portfolio/', 
   plugins: [react(), tailwindcss()],
 })
